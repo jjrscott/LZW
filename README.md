64311cb5147e73f49f6cd2399dccdbb0f6030a78
John Scott <john@jjrscott.com>
2022-09-09 08:26:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LZW
+
+From [Rosetta Code](https://rosettacode.org/wiki/LZW_compression):
+
+> The Lempel-Ziv-Welch (LZW) algorithm provides loss-less data compression.
+>
+> You can read a complete description of it in the [Wikipedia article](https://en.wikipedia.org/wiki/Lempel-Ziv-Welch) on the subject. It was patented, but it entered the public domain in 2004.
---
+++ Tests/LZWTests/LZWTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import LZW
+
+final class LZWTests: XCTestCase {
+    func testExample() throws {
+        
+        let original = "TOBEORNOTTOBEORTOBEORNOT"
+        
+        let compressed = LZW.compress(original)
+        
+        let decompressed = try XCTUnwrap(LZW.decompress(compressed))
+
+        XCTAssertEqual(original, decompressed)
+    }
+}
